6e7eaa8e96a1063a2e28aa4a0e5354831a4e3b47
arturevs <artur.rp2001@gmail.com>
2024-10-07 11:26:59 -0300

    final final commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
 function calculateChance() {
-    const distance = Number.parseFloat(document.getElementById("distance").value);
-    const weaponMultiplier = Number.parseFloat(document.getElementById("weaponMultiplier").value);
-    const bonus = Number.parseFloat(document.getElementById("bonus").value);
-    const penalty = Number.parseFloat(document.getElementById("penalty").value);
-
-
+    const distance = Number.parseFloat(document.getElementById("distance").value || 0);
+    const weaponMultiplier = Number.parseFloat(document.getElementById("weaponMultiplier").value || 0);
+    const bonus = Number.parseFloat(document.getElementById("bonus").value || 0);
+    const penalty = (getSelectedPenalties().length * 0.2) + 1
     const P = 95 / Math.exp(0.1 * (distance - 2) * weaponMultiplier * bonus * penalty);
 
 
     const chance = Math.min(95, P).toFixed(2);
 
-
-    document.getElementById("result").innerText = `Chance de Acerto: ${chance}%`;
-}
-//just a test
+    if(weaponMultiplier === 0 || bonus === 0)
+    {
+        document.getElementById("result").innerText = "Valores faltantes";
+    }
+    else
+    {
+        document.getElementById("result").innerText = `Chance de Acerto: ${chance ?? 0}%`;
+    }
+}   
+  
+  function getSelectedPenalties() {
+    const checkboxes = document.querySelectorAll('.dropdown-content input[type="checkbox"]');
+    const selectedPenalties = [];
+    checkboxes.forEach(function(checkbox) {
+      if (checkbox.checked) {
+        selectedPenalties.push(checkbox.value);
+      }
+    });
+    return selectedPenalties;
+  }
+  
+  // Example usage
+  document.querySelector('.dropdown button').addEventListener('click', function() {
+    console.log(getSelectedPenalties());
+  });
