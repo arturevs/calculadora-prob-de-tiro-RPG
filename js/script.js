f2f70e9f5bfbef516b9cdab6327cd8853c10b1bd
Artur Revorêdo Pinto <93280964+arturevs@users.noreply.github.com>
2025-01-04 12:06:46 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ function calculateChance() {
     }
     else
     {
-        document.getElementById("result").innerText = `Chance de Acerto: ${chance ?? 0}%`;
+        document.getElementById("result").innerText = `Valor necessário no D100 ${100-int(chance) ?? 0}%`;
     }
 }   
   
